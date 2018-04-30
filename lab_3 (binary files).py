7018567d49cbd0cc5c3f2157f6a44ecef43e50ba
Alexander Fedorov <31668662+FArekkusu@users.noreply.github.com>
2018-04-30 17:11:58 +0300

    Lab 3 (working with binary files)

---
+++ lab_3 (binary files).py
@@ -0,0 +1,30 @@
+def picture_magnify(source, target, times):
+    with open(source, "rb") as image:
+        bytes = bytearray(image.read())
+        header, body = bytes[:54], bytes[54:]
+
+    width, height = sum(header[18:22]), sum(header[22:26])
+    padding = width % 4
+    new_width, new_height = width * times, height * times
+    new_padding = new_width % 4
+
+    new_width, new_height = "{:>08}".format(hex(new_width)[2:]), "{:>08}".format(hex(new_height)[2:])
+    header[18:22] = [int(new_width[2*i:2*i+2], 16) for i in range(4)][::-1]
+    header[22:26] = [int(new_height[2*i:2*i+2], 16) for i in range(4)][::-1]
+
+    bytes = bytearray()
+    for _ in range(height):
+        for _ in range(width * 3): bytes.append(body.pop(0))
+        for _ in range(padding): body.pop(0)
+    body = [bytes[3*i:3*(i+1)] for i in range(width * height)]
+
+    new_body = bytearray()
+    for i in range(height * times):
+        for j in range(width):
+            new_body += bytearray(body[width * (i//times) + j] * times)
+        new_body += bytearray([0] * new_padding)
+
+    with open(target, "wb") as image:
+        image.write(header + new_body)
+
+picture_magnify("bmp.bmp", "new_picture.bmp", 3)
