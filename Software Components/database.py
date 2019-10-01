0142e90e334219d4e477d20bff5513550a90f228
Alexander Fedorov <31668662+FArekkusu@users.noreply.github.com>
2019-10-01 21:32:01 +0300

    Add files via upload

---
+++ Software Components/database.py
@@ -0,0 +1,77 @@
+import sqlite3
+
+
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class Connection(metaclass=Singleton):
+    def __init__(self, db=None):
+        self._db = sqlite3.connect(db, check_same_thread=False)
+
+    def __enter__(self):
+        return self._db.cursor()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._db.commit()
+
+
+class Query:
+    def __init__(self, table, action, filters, values):
+        self.table = table
+        self.action = action
+        self.filters = filters
+        self.values = values
+
+    def execute(self):
+        if self.action == "select":
+            with Connection() as c:
+                rows = c.execute(f"SELECT * FROM {self.table}")
+                if self.filters:
+                    rows = [r for r in rows if self.filters.check(r)]
+            return rows
+        if self.action == "insert":
+            with Connection() as c:
+                q = ",".join("?" * len(self.values))
+                query = f"INSERT INTO {self.table}({','.join(self.values.keys())}) VALUES ({q})"
+                c.execute(query, [*self.values.values()])
+        elif self.action == "delete":
+            with Connection() as c:
+                rows = c.execute(f"SELECT * FROM {self.table}")
+                columns = [x[0] for x in c.description]
+                rows = [{c: v for c, v in zip(columns, row)} for row in rows]
+                rows = [r["id"] for r in rows if self.filters.check(r)]
+                q = ",".join("?" * len(rows))
+                c.execute(f"DELETE FROM {self.table} WHERE id IN ({q})", rows)
+
+
+class QueryBuilder:
+    def __init__(self):
+        self._table = None
+        self._action = None
+        self._filters = None
+        self._values = None
+
+    def table(self, table):
+        self._table = table
+        return self
+
+    def action(self, action):
+        self._action = action
+        return self
+
+    def filters(self, filters):
+        self._filters = filters
+        return self
+
+    def values(self, values):
+        self._values = values
+        return self
+
+    def finalize(self):
+        return Query(self._table, self._action, self._filters, self._values)
---
+++ Software Components/filters.py
@@ -0,0 +1,52 @@
+from specification import Specification
+from datetime import datetime
+from operator import eq, lt, gt
+
+
+def comparison_specification_factory(op):
+    class Compare(Specification):
+        def __init__(self, field, value, *, function=lambda x: x):
+            self.field = field
+            self.value = value
+            self.function = function
+
+        def check(self, row):
+            return op(self.function(row[self.field]), self.value)
+
+    return Compare
+
+
+Equals = comparison_specification_factory(eq)
+Less = comparison_specification_factory(lt)
+Greater = comparison_specification_factory(gt)
+
+
+class Started(Specification):
+    def check(self, row):
+        field = "start_time"
+        today = datetime.today()
+        return (Less(field, today, function=parse_date) | Equals(field, today, function=parse_date)).check(row)
+
+
+class Finished(Specification):
+    def check(self, row):
+        return Less("end_time", datetime.today(), function=parse_date).check(row)
+
+
+class Paid(Specification):
+    def check(self, row):
+        return Equals("paid", True).check(row)
+
+
+class AwaitingPayment(Specification):
+    def check(self, row):
+        return (~Paid()).check(row)
+
+
+class Always(Specification):
+    def check(self, row):
+        return True
+
+
+def parse_date(value):
+    return datetime.strptime(value, "%d.%m.%Y")
